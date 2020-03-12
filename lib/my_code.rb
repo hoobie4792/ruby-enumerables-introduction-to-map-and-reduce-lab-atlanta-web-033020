d08c8d7f7b181c871fdbd768e5cb146b23cfda7c
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-12 17:21:11 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,8 +39,8 @@ def map_to_square(source_array)
   return arr
 end
 
-def reduce_to_total(source_array, starting_point = 0)
-  index = starting_point
+def reduce_to_total(source_array, starting_point = 1)
+  index = starting_point - 1
   total = 0
   
   while index < source_array.length do
