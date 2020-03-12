5d0a80d20021115276827ee4dfb27853d46dd07c
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-12 17:14:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,6 +5,7 @@ def map_to_negativize(source_array)
   
   while index < source_array.length do
     arr.push(source_array * -1)
+    index += 1
   end
   
   return arr
@@ -20,6 +21,7 @@ def map_to_double(source_array)
   
   while index < source_array.length do
     arr.push(source_array * 2)
+    index += 1
   end
   
   return arr
@@ -31,6 +33,7 @@ def map_to_square(source_array)
   
   while index < source_array.length do
     arr.push(source_array ** 2)
+    index += 1
   end
   
   return arr
@@ -42,6 +45,7 @@ def reduce_to_total(source_array, starting_point)
   
   while index < source_array.length do
     total += source_array[index]
+    index += 1
   end
   
   return total
@@ -53,9 +57,20 @@ def reduce_to_all_true(source_array)
   while index < source_array do
     if (source_array[index] == false)
       return false
+      index += 1
     end
   end
   
   return true
 end
 
+def reduce_to_any_true(source_array)
+  while index < source_array do
+    if (source_array[index] == true)
+      return true
+      index += 1
+    end
+  end
+  
+  return false
+end
