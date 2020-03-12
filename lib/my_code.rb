9393b016ed2e4606ad6bba033993c723fa6734a0
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-12 17:25:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,5 +52,12 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
+  index = 0
   
+  while index < source_array.length do
+    if !source_array[index]
+      return false
+  end
+  
+  return true
 end
