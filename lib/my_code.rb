1fc052fd078ee0e913ae2488d2392ab54b54602d
Brandon Brown <brandonbrown4792@gmail.com>
2020-03-12 17:08:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,12 @@
 # My Code here....
+def map_to_negativize(source_array)
+  index = 0
+  arr = []
+  
+  while index < source_array.length do
+    arr.push(source_array * -1)
+  end
+  
+  return arr
+end
+
